48565cef2913cd57cb2245f9e64730928f489fa0
AlexNuzum <alex.nuzum@suredatum.com>
2020-09-12 15:13:04 +0100

    Version 1 of funcitonality and UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,56 @@
+let socket = io(window.location.href.split("/")[2]),
+    canRequestCode = true,
+    template,clone,title,
+    folderTemplage,folderClone,folderTitle;
+
 document.onreadystatechange = function() {
     if (document.readyState == "complete") {
-        let selectFiles = document.getElementById("selectFiles");
-        selectFiles.addEventListener("change",function(e) {
-            console.log(selectFiles.files);
+        template = document.getElementById("file");
+        clone = template.content.querySelector(".file");
+        title = clone.querySelector(".title");
+        folderTemplate = document.getElementById("folder");
+        folderClone = folderTemplate.content.querySelector(".folder");
+        folderTitle = folderClone.querySelector(".title");
+
+        console.log(template,title,clone);
+
+        getDirectory();
+
+        document.getElementById("goUp")
+        .addEventListener("click",function() {
+            goUp();
         });
     }
 }
 
-function readURL(input,callback) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            return callback(e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
+function goUp() {
+    socket.emit("goUp");
+}
+
+function getDirectory() {
+    socket.emit("getDirectory");
+    let filesBox = document.getElementById("files"),
+        folderName = document.getElementById("folderName");
+    socket.on("folderContents",function(data) {
+        while(filesBox.firstChild) {
+            filesBox.removeChild(filesBox.firstChild);
+        }
+        folderName.innerText = data.dir;
+        data.folders.forEach(function(item) {
+            folderTitle.innerText = item;
+            let elm = folderClone.cloneNode(true);
+            filesBox.append(elm);
+            elm.addEventListener("click",function() {
+                socket.emit(socket.emit("getDirectory",{dir:elm.querySelector(".title").textContent}));
+            })
+        });
+        data.files.forEach(function(item) {
+            title.innerText = item;
+            let elm = clone.cloneNode(true);
+            filesBox.append(elm);
+            elm.querySelector("button").addEventListener("click",function() {
+                window.open("/?file="+elm.querySelector(".title").textContent);
+            });
+        });
+    });
 }
--- server.js
+++ server.js
@@ -1,10 +1,75 @@
-var express = require("express"),
-    app = express();
+let express = require("express"),
+    app = express(),
+    path = require("path"),
+    fs = require("fs"),
+    dir = __dirname,
+    res;
+
+let code;
 app.use(express.static(__dirname));
 app.get("/", function(req,res) {
-    
+    if(Object.keys(req.query).length !== 0) {
+        res.download(dir+"/"+req.query.file);
+    }else {
+        res.redirect("/public");
+    }
+});
+let server = app.listen(process.env.PORT|| 8080,function() {
+    console.log("Server is running");
 });
 
-app.listen(process.env.PORT|| 8081,function() {
-    console.log("Server is running");
+let io = require("socket.io").listen(server)
+
+io.on("connection",function(client) {
+    client.on("goUp",function() {
+        if(dir.substring(0, dir.lastIndexOf('\\')) != "") {
+            dir = dir.substring(0, dir.lastIndexOf('\\'));
+            getDirectory(dir);
+        }
+    });
+
+    client.on("getDirectory",function(data) {
+        let dd = data != undefined ? dir+"\\"+data.dir : __dirname;
+
+        getDirectory(dd);
+    });
+
+    client.on("downloadFile",function(data) {
+        const file = dir+"\\"+data.file;
+        console.log(res);
+    })
+
+    function getDirectory(DIR) {
+        var fls = [], folders = [];
+        var f = 0, fldr = 0, num = 0;
+
+        fs.readdir(DIR,function(error,files) {
+            try {
+                if(error) {
+                    console.log(error)
+                    io.to(client.id).emit("folderContents",{empty:true});
+                }
+                if(files.length == 0) {
+                    io.to(client.id).emit("folderContents",{empty:true});
+                }
+                files.forEach(function(file,index) {
+                    fs.lstat(DIR+"/"+file,
+                    function(error,stats) {
+                        num++
+                        if(!error && stats.isDirectory()) {
+                            folders[fldr++] = file;
+                        }else {
+                            fls[f++] = file;
+                        }
+                        if(num == files.length) {
+                            dir = DIR;
+                            io.to(client.id).emit("folderContents",{files:fls,folders:folders,dir:DIR});
+                        }
+                    });
+                });
+            }catch(e) {
+                console.log(e);
+            }
+        });
+    }
 });
